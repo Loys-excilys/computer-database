8d3318414b225536e17ac7b89e93089be2b75f18
Loys-excilys <lberthelier@excilys.com>
2021-04-14 16:45:04 +0200

    correction dependance

--- webapp/src/main/java/com/excilys/computer/database/api/APILogin.java
+++ webapp/src/main/java/com/excilys/computer/database/api/APILogin.java
@@ -6,6 +6,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 
 import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,43 +22,46 @@ import com.excilys.computer.database.service.ServiceUser;
 @RestController
 @RequestMapping("/login")
 public class APILogin {
-	
+
 	@Autowired
 	private ServiceUser serviceUser;
 
 	@PostMapping(consumes = "application/json", produces = "application/json")
-	public ResponseEntity<String> login(@RequestBody LoginDTO login) {	
+	public ResponseEntity<String> login(@RequestBody LoginDTO login) {
 		try {
-			if(this.serviceUser.login(login.getUsername(), login.getPassword())) {
+			if (this.serviceUser.login(login.getUsername(), login.getPassword())) {
 				return new ResponseEntity<>("authentification ok", HttpStatus.OK);
-			}else {
+			} else {
 				return new ResponseEntity<>("authentification failed", HttpStatus.NOT_ACCEPTABLE);
 			}
 		} catch (ErrorSaisieUser e) {
 			e.formatEntry();
 			return new ResponseEntity<>("authentification failed", HttpStatus.NOT_ACCEPTABLE);
-		}		
+		}
 	}
+
+	@PostMapping(value = "/Oauth", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<String> Oauth(@RequestBody LoginDTO login) {
+		StringBuilder builder = new StringBuilder();
+		try {
+			URL url = new URL("http://localhost:8080/webapp/oauth/token?username=" + login.getUsername() + "&password="
+					+ login.getPassword() + "&grant_type=password&client_id=clientIdPassword");
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("POST");
+			conn.addRequestProperty("content-type", "application/json");
+			conn.setDoOutput(true);
 	
+			conn.setRequestProperty("Authorization", "Basic Y2xpZW50SWRQYXNzd29yZDpzZWNyZXQ=");
+			conn.connect();
 	
-	@PostMapping(value="/Oauth", consumes = "application/json", produces = "application/json")
-	public ResponseEntity<String> Oauth(@RequestBody LoginDTO login) throws IOException{
-		URL url = new URL("http://localhost:8080/webapp/oauth/token?username=" + login.getUsername() + "&password=" + login.getPassword() + "&grant_type=password&client_id=clientIdPassword");
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.addRequestProperty("content-type", "application/json");
-		conn.setDoOutput(true);
-		
-		conn.setRequestProperty("Authorization", "Basic Y2xpZW50SWRQYXNzd29yZDpzZWNyZXQ=");
-		conn.connect();
-		
-		BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
-		 
-	    StringBuilder builder = new StringBuilder();
-	    for(String line = reader.readLine(); line != null; line = reader.readLine()) {
-	      builder.append(line + "\n");
-	    }
-	    
-	    return new ResponseEntity<>(builder.toString(), HttpStatus.OK);	
+			BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), "utf-8"));
+	
+			for (String line = reader.readLine(); line != null; line = reader.readLine()) {
+				builder.append(line + "\n");
+			}
+		} catch (IOException error) {
+			return new ResponseEntity<>("invalid login", HttpStatus.BAD_REQUEST);
+		}
+		return new ResponseEntity<>(builder.toString(), HttpStatus.OK);
 	}
 }
