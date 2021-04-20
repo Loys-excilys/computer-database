2119eab0ce7974d41ded7618bb12948e846d52f7
Loys-excilys <lberthelier@excilys.com>
2021-04-20 23:33:39 +0200

    correction update computer company

--- persistence/src/main/java/com/excilys/computer/database/dao/DAOComputer.java
+++ persistence/src/main/java/com/excilys/computer/database/dao/DAOComputer.java
@@ -287,7 +287,7 @@ public class DAOComputer {
 		query.set("name", computerDTO.getName());
 		query.set("introduced", MappeurDate.localDateToDate(computerDTO.getIntroduced()));
 		query.set("discontinued", MappeurDate.localDateToDate(computerDTO.getDiscontinued()));
-		query.set("company", computerDTO.getCompany().getId());
+		query.set("company", computerDTO.getCompany() != null ? computerDTO.getCompany().getId() : null);
 		query.where(cb.equal(rootComputer.get("id"), computerDTO.getId()));
 
 		if(em.createQuery(query).executeUpdate() < 1){
